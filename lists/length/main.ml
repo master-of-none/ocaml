b623c4369e6fdd18f937ac772b8592e4a990db91
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-05-28 23:08:11 -0700

    Some documentation

--- lists/length/main.ml
+++ lists/length/main.ml
@@ -1,9 +1,10 @@
 let rec length lst = 
   match lst with
     | [] -> 0
-    | _ :: t -> 1 + length t ;;
+    | _ :: t -> 1 + length t ;; (* The value h gives error, hence we have to replace it with _*)
 
 let result = length [1; 2; 3; 4; 5] in
+print_string "The length of the list is: ";
 print_int result;
 print_newline() ;;
 
