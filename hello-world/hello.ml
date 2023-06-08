90cd2e703ef89656ad33cc48699266b4f696e056
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-08 15:28:28 -0700

    Folder fixes

--- hello-world/hello.ml
+++
@@ -1,3 +0,0 @@
-let _ = print_endline "Hello World"
-let _ = print_endline "Some lines"
-let _ = print_endline "Some lines again"
--- lists/sum/main.ml
+++
@@ -1,8 +0,0 @@
-let rec sum lst =
-  match lst with
-    | [] -> 0
-    | h :: t -> h + sum t;;
-
-let result = sum [1; 2; 3; 4; 5] in
-  print_int result;
-  print_newline() ;;
