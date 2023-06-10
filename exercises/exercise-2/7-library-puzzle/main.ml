9a5c75d0aff4d97eb4c4bfcaec64d2959249e706
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-10 10:35:46 -0700

    library puzzle part 2

--- exercises/exercise-2/7-library-puzzle/main.ml
+++ exercises/exercise-2/7-library-puzzle/main.ml
@@ -3,8 +3,16 @@ let last_element list =
   List.hd reversed
 ;;
 
-let my_list = [1; 2; 3; 4; 5];;
+let my_list1 = [1; 2; 3; 4; 5];;
 
-let last_val = last_element my_list;;
+let last_val = last_element my_list1;;
 print_endline "Last value in the list is"; print_int last_val;;
+print_newline();;
 
+let check_zero list = 
+  List.mem 0 list
+;;
+
+let my_list2 = [1; 2; 3;];;
+let result = check_zero my_list2;;
+print_string "There is a zero present in the list "; print_string (string_of_bool result);;
