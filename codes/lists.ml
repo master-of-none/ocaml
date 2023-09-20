b1017cdd727fa6c3674a06133114e185efe3722f
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-09-20 11:37:07 -0700

    2 new functions

--- codes/lists.ml
+++ codes/lists.ml
@@ -15,3 +15,18 @@ let rec append lst1 lst2 =
   match lst1 with
   | [] -> lst2
   | h :: t -> h :: append t lst2
+
+(** Empty List *)
+let empty lst =
+  match lst with
+  | [] -> true
+  | h :: t -> false
+
+(** Another way of empty list *)
+let empty_alt lst = lst = []
+
+(** Incrementing all the elements in the list *)
+let rec inc lst =
+  match lst with
+  | [] -> []
+  | h :: t -> (h + 1) :: inc t
