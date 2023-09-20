e935dd6a3c69a2c8f4a4a7e07064f8765331a168
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-09-19 22:31:51 -0700

    appending lists

--- codes/lists.ml
+++ codes/lists.ml
@@ -1,11 +1,17 @@
-(** Sum of all the elements in the list*)
+(** Sum of all the elements in the list *)
 let rec sum lst =
   match lst with
   | [] -> 0
   | h :: t -> h + sum t
 
-(** Length of the list*)
+(** Length of the list *)
 let rec length lst =
   match lst with
   | [] -> 0
   | _ :: t -> 1 + length t
+
+(** Appending the list *)
+let rec append lst1 lst2 =
+  match lst1 with
+  | [] -> lst2
+  | h :: t -> h :: append t lst2
