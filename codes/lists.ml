40b72393ec758f88f41d1591870fa67981faea1a
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-09-19 19:07:03 -0700

    length of list

--- codes/lists.ml
+++ codes/lists.ml
@@ -1,1 +1,5 @@
+(** Sum of all the elements in the list*)
 let rec sum lst = match lst with [] -> 0 | h :: t -> h + sum t
+
+(** Length of the list*)
+let rec length lst = match lst with [] -> 0 | _ :: t -> 1 + length t
