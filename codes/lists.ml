59b419c3ee1223b24aa3ece9952a5cc8004d1d3e
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-09-19 20:44:01 -0700

    formatting

--- codes/lists.ml
+++ codes/lists.ml
@@ -1,5 +1,11 @@
 (** Sum of all the elements in the list*)
-let rec sum lst = match lst with [] -> 0 | h :: t -> h + sum t
+let rec sum lst =
+  match lst with
+  | [] -> 0
+  | h :: t -> h + sum t
 
 (** Length of the list*)
-let rec length lst = match lst with [] -> 0 | _ :: t -> 1 + length t
+let rec length lst =
+  match lst with
+  | [] -> 0
+  | _ :: t -> 1 + length t
