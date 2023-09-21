2ca5b4d6a8ab905c3456295e150a96d824e20088
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-09-20 19:35:50 -0700

    trees

---
+++ codes/tree_tuple.ml
@@ -0,0 +1,9 @@
+type 'a tree =
+  | Leaf
+  | Node of 'a * 'a tree * 'a tree
+
+let t =
+  Node
+    ( 4
+    , Node (2, Node (1, Leaf, Leaf), Node (3, Leaf, Leaf))
+    , Node (5, Node (6, Leaf, Leaf), Node (7, Leaf, Leaf)) )
