7fa6ba1f54717029fca0bb33555dc4091c105d79
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-11-01 20:00:55 -0700

    Formatted code

--- exercise-ocaml/tail_of_list.ml
+++ exercise-ocaml/tail_of_list.ml
@@ -1,11 +1,11 @@
-let rec tail_of_list lst = 
+let rec tail_of_list lst =
   match lst with
   | [] -> None
-  | [x] -> Some(x)
-  | _ :: t ->tail_of_list t
+  | [ x ] -> Some x
+  | _ :: t -> tail_of_list t
 
-  let () =
-    let my_list = [1] in
-    match tail_of_list my_list with
-    | None -> print_endline "Empty list"
-    | Some(x) -> print_endline ("Tail of the list: " ^ string_of_int x)
+let () =
+  let my_list = [ 1 ] in
+  match tail_of_list my_list with
+  | None -> print_endline "Empty list"
+  | Some x -> print_endline ("Tail of the list: " ^ string_of_int x)
