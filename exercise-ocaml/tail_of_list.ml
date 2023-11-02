312f3b2446f053571aba9d50c135317062ef6056
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-11-01 19:25:01 -0700

    Added code for tail of list

--- exercise-ocaml/tail_of_list.ml
+++ exercise-ocaml/tail_of_list.ml
@@ -1,1 +1,11 @@
-  print_string "Hello World"
+let rec tail_of_list lst = 
+  match lst with
+  | [] -> None
+  | [x] -> Some(x)
+  | _ :: t ->tail_of_list t
+
+  let () =
+    let my_list = [1] in
+    match tail_of_list my_list with
+    | None -> print_endline "Empty list"
+    | Some(x) -> print_endline ("Tail of the list: " ^ string_of_int x)
