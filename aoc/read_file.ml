7326dc581393e7cdcb74ddb316a4d491f5243c6e
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-11-30 19:48:19 -0500

    Updated Formatting

--- aoc/read_file.ml
+++ aoc/read_file.ml
@@ -2,4 +2,6 @@ open Core
 
 let r file = In_channel.read_lines file
 
-let () = let lines = r "file.txt" in List.iter ~f:print_endline lines
+let () =
+  let lines = r "file.txt" in
+  List.iter ~f:print_endline lines
