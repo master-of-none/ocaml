0ca84c114a2ef7bcc59dcd4e63307fded1d19531
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-12-02 12:12:50 -0500

    Setup for running each day's code

--- aoc/main.ml
+++ aoc/main.ml
@@ -1,1 +1,12 @@
-let () = Day1.solve "day1/input.txt"
+let () =
+if Array.length Sys.argv < 2 then
+  Printf.printf "Usage: %s <day_number>\n" Sys.argv.(0)
+else
+  let day = Sys.argv.(1) in 
+  Printf.printf "Running Day %s Solution\n" day;
+  
+
+match day with
+| "1" -> Day1.solve "day1/input.txt"
+| "2" -> Day2.solve "day2/input.txt"
+| _ -> Printf.printf "Unknown Day\n"
