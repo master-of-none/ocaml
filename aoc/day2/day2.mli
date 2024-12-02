437a61337600411d8b14de4c28bb3cddc5f54de6
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-12-02 12:12:30 -0500

    Day2 code

---
+++ aoc/day2/day2.mli
@@ -0,0 +1,5 @@
+val split_lines : string list -> int list list
+val safe_report : int list -> bool
+val puzzle1 : int list list -> int
+val puzzle2 : int list list -> int
+val solve : string -> unit
