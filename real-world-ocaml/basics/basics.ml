a3057d75efb1642141c3adaacd0ad14d8aae5f6c
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2025-07-02 11:35:08 +0530

    Some Code

--- real-world-ocaml/basics/basics.ml
+++ real-world-ocaml/basics/basics.ml
@@ -1,3 +1,6 @@
+open Base
+open Stdio
+
 let () = Stdio.print_endline "Hello"
 
 (** Arrays *)
@@ -9,7 +12,7 @@ let rec sum l =
   | [] -> 0
   | hd :: tl -> hd + sum tl
 
-let () = print_endline (string_of_int (sum [ 1; 2; 3; 4 ]))
+let () = print_endline (Int.to_string (sum [ 1; 2; 3; 4 ]))
 
 let rec remove_sequential_duplicates list =
   match list with
@@ -20,3 +23,29 @@ let rec remove_sequential_duplicates list =
       else first :: remove_sequential_duplicates (second :: tl)
 
 let _ = remove_sequential_duplicates [ 1; 1; 2; 3; 4 ]
+
+let languages = "OCaml, Python, C++";;
+let dashed_languages = 
+  let language_list = String.split languages ~on: ',' in
+  let trimmed_list = List.map ~f: String.strip language_list in
+  String.concat ~sep: "-" trimmed_list
+
+(**? Other Way *)
+let dashed_languages_pipe = 
+  languages
+  |> String.split ~on: ','
+  |> List.map ~f: String.strip
+  |> String.concat ~sep: "-"
+
+let () = print_endline ("Dashed Languages: " ^  dashed_languages_pipe)
+
+(* let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None -> accum
+  | Some x -> read_and_accumulate (accum +. Float.of_string x)
+
+let () =
+  print_endline "Enter numbers and enter <C-d> to stop the code";
+  printf "Total: %F\n" (read_and_accumulate 0.)
+ *)
