9384ffc6ec46545e12c807ea7c9f1d44279e8b5f
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2025-07-13 16:17:05 +0530

    Mandelbrot and Sieve 🎶

---
+++ programming_caml/beginnings/sieve.ml
@@ -0,0 +1,25 @@
+let max =
+  Printf.printf "Enter interger upto which prime number is to be calculated\n";
+  read_int ()
+
+let prime = Array.make (max + 1) true
+
+let () =
+  prime.(0) <- false;
+  prime.(1) <- false;
+
+  let limit = truncate (sqrt (float max)) in
+  for n = 2 to limit do
+    if prime.(n) then begin
+      let m = ref (n * n) in
+      while !m <= max do
+        prime.(!m) <- false;
+        m := !m + n
+      done
+    end
+  done
+
+let () =
+  for n = 2 to max do
+    if prime.(n) then Printf.printf "%d\n" n
+  done
